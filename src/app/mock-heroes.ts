06894f80f8bea87505fb018deb7bb1de2a41332f
manjitwaikhom <manjitsinghwk@gmail.com>
2018-06-30 14:53:04 +0530

    Tour of heros

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jordan Peterson' },
+  { id: 12, name: 'Richard Dawkins' },
+  { id: 13, name: 'Elon Musk' },
+  { id: 14, name: 'Steve Jobs' },
+  { id: 15, name: 'Sam Harris' },
+  { id: 16, name: 'Christopher Hitchens' },
+  { id: 17, name: 'Neil deGrasse Tyson' },
+  { id: 18, name: 'Noam Chomsky' },
+  { id: 19, name: 'Stephen Fry' },
+  { id: 20, name: 'Lawrence Krauss' }
+];
